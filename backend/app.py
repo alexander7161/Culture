bc579f05b488bb0b173e1334d5503ca47bfa7c74
atomract <46929915+atomract@users.noreply.github.com>
2019-07-20 17:15:15 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,4 +7,8 @@ firebase_admin.initialize_app()
 acities = firestore.client().collection('acities')
 
 @app.route('/cities', methods=['POST'])
-
+def create_cities():
+  req = flask.request.json
+  cities = acities.document()
+  cities.set(req)
+  return flask.jsonify({'id': cities.id}), 201
