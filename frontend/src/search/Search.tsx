49c2a5e280d0e5d1f33edabfb36a9110e9feb326
Laquesisa <dana.shmaria@gmail.com>
2019-07-20 16:14:28 +0200

    Added page Search

---
+++ frontend/src/search/Search.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Search.css';
+
+const Search: React.FC = () => {
+  return (
+    <div>
+        <p>
+            hello
+        </p>
+    </div>
+  );
+}
+
+export default Search;
